76cc5609bd790d5dc8b03635714160b1f22ee1b2
Ben <benoit.zeller@gmail.com>
2021-11-13 20:39:14 +0100

    fix(themes): add should_generate_new_friendly_id? method

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,10 @@
 class Theme < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  private
+
+  def should_generate_new_friendly_id?
+    slug.blank? || title_changed?
+  end
 end
