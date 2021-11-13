25c0708398784c61df8bf92c6fbcfa67d3ba8351
Ben <benoit.zeller@gmail.com>
2021-11-13 14:20:28 +0100

    feat(app): update rails generator config

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module Website
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.generators do |g|
+      g.test_framework nil
+      g.helper false
+    end
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
