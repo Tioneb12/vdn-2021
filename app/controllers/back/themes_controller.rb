f5df1ada6b2e8f416de6d931e3871040037069a7
Ben <benoit.zeller@gmail.com>
2021-11-13 20:38:52 +0100

    fix(themes): create crud & form

--- app/controllers/back/themes_controller.rb
+++ app/controllers/back/themes_controller.rb
@@ -1,17 +1,55 @@
-class Back::ThemesController < ApplicationController
+class Back::ThemesController < BackController
+  before_action :theme, only: [:edit, :update, :destroy]
+
   def index
     @themes = Theme.all
   end
 
   def new
+    @theme = Theme.new
   end
 
   def create
+    @theme = Theme.new(theme_params)
+    if @theme.save
+      redirect_to admin_themes_path
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @theme.update(theme_params)
+      redirect_to admin_themes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @theme.destroy!
+    redirect_to admin_themes_path
+  end
+
+  private
+
+  def theme
+    @theme = Theme.friendly.find(params[:id])
+  end
+
+  def theme_params
+    params.require(:theme).permit(
+      :title,
+      :description,
+      :slug,
+      :meta_robots_index,
+      :meta_robots_follow,
+      :meta_title,
+      :meta_description,
+      :statut
+    )
   end
 end
+
