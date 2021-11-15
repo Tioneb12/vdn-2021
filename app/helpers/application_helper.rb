860b5bf4f806900cdab9300bc5bf4b664a906858
Ben <benoit.zeller@gmail.com>
2021-11-15 20:00:41 +0100

    fix(tinymce): add uploader image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def robots(index, follow)
+    if index == true && follow == false
+      'index, nofollow'
+    elsif index == false && follow == true
+      'noindex, follow'
+    else
+      "none"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
 
+  post 'uploader/image', to: 'uploader#image'
   resources :themes, only: [:show]
 
   scope 'back', module: 'back', as: 'admin' do
