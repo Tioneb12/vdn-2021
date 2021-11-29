fcf25b5fc6c01ca063bb21e86dafdf42daccbfbe
Ben <benoit.zeller@gmail.com>
2021-11-29 18:55:29 +0100

    fix(post): add default value to counter post

---
+++ db/migrate/20211129174914_add_default_value_to_post_count.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToPostCount < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :posts, :comments_count, 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_201732) do
+ActiveRecord::Schema.define(version: 2021_11_29_174914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_201732) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "comments_count"
+    t.integer "comments_count", default: 0
     t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
